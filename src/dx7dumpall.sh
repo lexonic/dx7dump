7981bfd27dbc52b3d8950dd3323a288e4d455fe9
lexonic <belex@ayeacademy.com>
2023-04-24 20:32:23 +0200

    dx7dumpall no longer prints filenames -> this function will be implemented in dx7dump

--- src/dx7dumpall.sh
+++ src/dx7dumpall.sh
@@ -7,7 +7,10 @@
 #   any other arguments (short and long forms) will be handed over to dx7dump
 #
 # Revision history :
-#   28/02/2023, V1.0 - Creation by B.Lex
+#   28/02/2023, v1.0 - Creation by B.Lex
+#   20/04/2023, added help-screen
+#
+# License: GPLv3+
 # ---------------------------------------------------
 
 # dx7dump arguments
@@ -19,15 +22,21 @@ searchpath="."
 
 printbank () {
 	# remove "./" from the beginning of the path
-	pathname="${1/#\.\//}"
-	echo "$pathname"
-	dx7dump $dx7_opt "$1"
-	echo ""
+	filename="${1/#\.\//}"
+	#echo "$filename"
+	dx7dump $dx7_opt "$filename"
+	#echo ""
 }
 
 # argument parsing
 while [ -n "$1" ]; do
 	case "$1" in
+		-h|--help)	echo -e "dx7dump wrapper: Print lists of all sound banks recursively.\n"
+			echo "Usage: $0 [OPTIONS for dx7dump] [PATH]"
+			echo "    An argument WITHOUT a leading '-' or '--' is interpreted as the path where the recursive search will start."
+			echo "    All other arguments are passed to the dx7dump utility."
+			exit
+			;;
 		-p) # special case for -p with argument
 			dx7_opt+="$1 "
 			shift
